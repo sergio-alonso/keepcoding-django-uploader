3f8c48eff4a69676a5ff15f8372d785fc555d10c
Sergio Alonso <sergio@sergioalonso.es>
2017-05-10 01:29:30 +0200

    api router

---
+++ api/api.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
+from api.viewsets import UploadViewSet
+
+router = routers.DefaultRouter()
+router.register('entry', UploadViewSet)
