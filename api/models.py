b4d359a961440a24a2714b64a90770aa82124781
Sergio Alonso <sergio@sergioalonso.es>
2017-05-10 00:38:22 +0200

    Add image model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Image(models.Model):
+    file = models.FileField(upload_to=settings.MEDIA_ROOT)
